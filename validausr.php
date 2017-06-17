979c02d18bec2bdac64df8af6c0ab754f8a93052
alencaroliveira-luizalabs <an_oliveira@luizalabs.com>
2017-06-17 14:54:00 -0300

    ok

--- validausr.php
+++ validausr.php
@@ -11,11 +11,11 @@ $tempo_valido = 30; // numero de minutos que a sess�o
 $unixtime_valido = (strtotime('NOW') + 3600) - ($tempo_valido * 60); 
 
 $deleta = sprintf("DELETE FROM usr_online WHERE datetime < '%s'",$unixtime_valido);
-$result = mysqli_query($deleta) 
-	 		or die('\nErro validando sessão 1: ' . mysqli_error()); 
+$result = mysqli_query($link,$deleta) 
+	 		or die('\nErro validando sessão 1: ' . mysqli_error($link)); 
 
 $sql = sprintf("SELECT * FROM usr_online WHERE sessao='%s' AND datetime > '%s'",$_ID,$unixtime_valido);
-$result = mysqli_query($sql)
+$result = mysqli_query($link,$sql)
 	 		or die('\nErro validando sessão 3: ' . mysqli_error()); 
 if (mysqli_num_rows($result) > 0) { 
    $row = mysqli_fetch_array($result); 
@@ -23,7 +23,7 @@ if (mysqli_num_rows($result) > 0) {
    $_SESSION['userid'] = $row['userid'];
    $unixtime = (strtotime('NOW') + 3600); 
    $sql = sprintf("UPDATE usr_online SET datetime='%s' WHERE sessao='%s'",$unixtime,$_ID); 
-   $result = mysqli_query($sql)
+   $result = mysqli_query($link,$sql)
 	 		or die('\nErro validando sessão 2: ' . mysqli_error()); 
 //   mysqli_free_result($result);
 } else { 
